0562822a6196ae675a02692c2637611cbb99bf56
hadrielyharrizon <hadrielyharrizon3@gmail.com>
2026-03-31 17:40:59 -0300

    Adiciona níveis de dificuldade e sistema de pontos

--- home/jogos/guessing.py
+++ home/jogos/guessing.py
@@ -3,25 +3,52 @@ print('* Jogo da Adivinhação *')
 print('************')
 
 secretnumber = 42
-attemps = 3
-rounds = 1
+level1 = 20
+level2 = 10
+level3 = 5
 
-while rounds <= attemps:
-    print(f'{rounds}º tentativa de {attemps}.')
-    guessing = int(input('Digite um número: '))
-    print(f'Você digitou: {guessing}')
+points = 1000
 
-    correct = guessing == secretnumber
-    maior = guessing > secretnumber
-    menor = guessing < secretnumber
+level = int(input('Escolha um nível: (1) Fácil  (2) Médio  (3) Difícil ' ))
 
-    if correct:
+if level == 1:
+    attemps = level1
+    print(f'No nível fácil você terá: {level1} tentativas.')
+elif level == 2:
+    attemps = level2
+    print(f'No nível médio você terá: {level2} tentativas.')
+elif level == 3:
+    attemps = level3
+    print(f'No nível difícil você terá: {level3} tentativas.')
+else:
+    print('Nível inválido. Digite 1, 2 ou 3.')
+    exit()
+
+    
+for rounds in range (1, attemps+1):
+    guess = int(input(f'{rounds}ª tentativa. Digite um número: '))
+
+
+    if guess == secretnumber:
         print('Você acertou!')
-    elif maior:
+        break
+    else:
+        lose = abs(guess - secretnumber)
+        points -= lose
+    
+    if guess > secretnumber:
         print('Você errou! O seu chute foi maior que o número secreto.')
-    elif menor:
+    else:
         print('Você errou! O seu chute foi menor que o número secreto.')
 
-    rounds = rounds + 1
+    print(f'Você perdeu {lose} pontos.')
+    print(f'Sua pontuação atual é: {points}')
 
+else:
+    print('Você perdeu.')
+    
+
+print(f'Pontuação: {points}')
 print('Fim de jogo.')
+
+    
