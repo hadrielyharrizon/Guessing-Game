3c67d9e1ee51278cafb67982757096c19350e18d
hadrielyharrizon <hadrielyharrizon3@gmail.com>
2026-03-31 17:00:16 -0300

    Adiciona tentativas

--- home/jogos/guessing.py
+++ home/jogos/guessing.py
@@ -3,17 +3,25 @@ print('* Jogo da Adivinhação *')
 print('************')
 
 secretnumber = 42
+attemps = 3
+rounds = 1
 
-guessing = int(input('Digite um número: '))
-print(f'Você digitou: {guessing}')
+while rounds <= attemps:
+    print(f'{rounds}º tentativa de {attemps}.')
+    guessing = int(input('Digite um número: '))
+    print(f'Você digitou: {guessing}')
 
-correct = guessing == secretnumber
-maior = guessing > secretnumber
-menor = guessing < secretnumber
+    correct = guessing == secretnumber
+    maior = guessing > secretnumber
+    menor = guessing < secretnumber
 
-if correct:
-    print('Você acertou!')
-elif maior:
-    print('Você errou! O seu chute foi maior que o número secreto.')
-elif menor:
-    print('Você errou! O seu chute foi menor que o número secreto.')
+    if correct:
+        print('Você acertou!')
+    elif maior:
+        print('Você errou! O seu chute foi maior que o número secreto.')
+    elif menor:
+        print('Você errou! O seu chute foi menor que o número secreto.')
+
+    rounds = rounds + 1
+
+print('Fim de jogo.')
